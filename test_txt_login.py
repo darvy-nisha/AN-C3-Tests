c5a6632ef25a573c1ffe1dc5ab8172c604fd99ec
darvy-nisha <darvy.nisha@gmail.com>
2026-01-15 19:26:26 +0530

    test

--- test_txt_login.py
+++ test_txt_login.py
@@ -1,43 +1,30 @@
 import pytest
-from PageObjects.LoginPage import Login
-import time
+from LoginPage import Login
 
 
 
 @pytest.mark.usefixtures("setup")
-class Testlogin:
+class TestLogin:
+    driver = None
 
-
-
-    def test_001(self):
-        lg=Login(self.driver)
+    def test_001_valid_login(self):
+        lg = Login(self.driver)
         lg.input_username("Admin")
         lg.input_password("Admin123")
         lg.click_login()
-        time.sleep(10)
+        # (assert dashboard later)
 
-
-
-    def test_002(self):
+    def test_002_invalid_username(self):
         lg = Login(self.driver)
         lg.input_username("Amin")
         lg.input_password("Admin123")
         lg.click_login()
 
-        time.sleep(10)
-        if 'Invalid credentials' in lg.invalid_mssg():
-            assert True
-        else:
-            assert False
+        assert "Invalid credentials" in lg.invalid_mssg()
 
-
-    def test_003(self):
+    def test_003_invalid_password(self):
         lg = Login(self.driver)
         lg.input_username("Admin")
         lg.input_password("Admin12")
         lg.click_login()
-        time.sleep(10)
-        if 'Invalid credentials' in lg.invalid_mssg():
-            assert True
-        else:
-            assert False
+        assert 'Invalid credentials' in lg.invalid_mssg(), "Failed to login"
