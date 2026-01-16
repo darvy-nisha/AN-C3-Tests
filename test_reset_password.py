7bd633e4fe2bb1d2c0b669743cc93d07b3ef8454
darvy-nisha <darvy.nisha@gmail.com>
2026-01-16 20:07:37 +0530

    development for test_reset_password test file

--- test_reset_password.py
+++ test_reset_password.py
@@ -1,6 +1,5 @@
 import pytest
 from ResetPasswordPage import ResetPasswordPage
-from LoginPage import LoginPage
 from logger_config import get_logger
 
 logger = get_logger()
@@ -8,19 +7,23 @@ logger = get_logger()
 
 @pytest.mark.usefixtures("setup")
 class TestLogin:
+    """
+    Test cases related to login and reset password functionality.
+    """
+
     driver = None
     reset_page: ResetPasswordPage
-    login_page: LoginPage
 
     def test_reset_password(self):
-        logger.info("Test started: Valid Login")
+        logger.info("Test started: Reset Password flow")
+
         self.reset_page = ResetPasswordPage(self.driver)
 
-        assert self.reset_page.click_forget_password_button(), "Failed to click forget button"
-        logger.info(f"Successfully clicked forget button")
+        assert self.reset_page.click_forget_password_button(), "Failed to click the 'Forgot Password' button"
+        logger.info("Successfully clicked the 'Forgot Password' button")
 
-        assert self.reset_page.enter_username("Admin"), "Failed to enter username"
-        logger.info(f"Successfully entered username")
+        assert self.reset_page.enter_username("Admin"), "Failed to enter the username"
+        logger.info("Successfully entered the username")
 
-        assert self.reset_page.click_reset_password_button(), "Failed to click reset button"
-        logger.info(f"Successfully clicked reset button")
+        assert self.reset_page.click_reset_password_button(), "Failed to click the reset password button"
+        logger.info("Successfully clicked the reset password button")
--- test_txt_login.py
+++ test_txt_login.py
@@ -11,33 +11,33 @@ class TestLogin:
     def test_001_valid_login(self):
         logger.info("Test started: Valid Login")
 
-        lg = LoginPage(self.driver)
+        login_page = LoginPage(self.driver)
 
-        assert lg.input_username(username="Admin"), "Failed to input username"
+        assert login_page.input_username(username="Admin"), "Failed to input username"
         logger.info("Username entered successfully")
 
-        assert lg.input_password(password="Admin123"), "Failed to input password"
+        assert login_page.input_password(password="Admin123"), "Failed to input password"
         logger.info("Password entered successfully")
 
-        assert lg.click_login(), "Failed to click login"
+        assert login_page.click_login(), "Failed to click login"
         logger.info("Login successful")
 
     def test_002_invalid_username(self):
         logger.info("Test started: Invalid Username")
 
-        lg = Login(self.driver)
+        login_page = LoginPage(self.driver)
 
-        assert lg.input_username("Amin"), "Failed to input invalid username"
+        assert login_page.input_username("Amin"), "Failed to input invalid username"
         logger.info("Invalid username entered")
 
-        assert lg.input_password("Admin123"), "Failed to input password"
+        assert login_page.input_password("Admin123"), "Failed to input password"
         logger.info("Password entered successfully")
 
-        assert lg.click_login(), "Failed to click login"
+        assert login_page.click_login(), "Failed to click login"
         logger.info("Login button clicked")
 
 
-        assert "Invalid credentials" in lg.invalid_mssg(), "Failed to input invalid username"
+        assert "Invalid credentials" in login_page.invalid_mssg(), "Failed to input invalid username"
         logger.info("Invalid username entered")
 
 
